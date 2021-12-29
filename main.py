b22b5b80b33a970e98f7136face00fbbeb6c78d2
AlWe <defel@users.noreply.github.com>
2021-12-29 13:25:04 +0100

    ✨: init - replace demo by pwdhash

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
 logger = logging.getLogger(__name__)
 
 
-class DemoExtension(Extension):
+class PwdHashExtension(Extension):
 
     def __init__(self):
-        super(DemoExtension, self).__init__()
+        super(PwdHashExtension, self).__init__()
         self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
         self.subscribe(ItemEnterEvent, ItemEnterEventListener())
 
@@ -46,4 +46,4 @@ class ItemEnterEventListener(EventListener):
 
 
 if __name__ == '__main__':
-    DemoExtension().run()
+    PwdHashExtension().run()
